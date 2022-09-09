5d68ea03e2fe7e717e7d3d765f4ca39612b61bac
reedsutliff <sutliff.reed@gmail.com>
2022-09-09 10:03:01 -0400

    multi-targeting net6-win

--- Base.cs
+++ Base.cs
@@ -17,15 +17,12 @@ using Impower.Office365.Authentication;
 
 namespace Impower.Office365
 {
-    public abstract class Office365Activity : AsyncTaskCodeActivity
+    public abstract class Office365Activity : UiPath.Shared.Activities.AsyncTaskCodeActivity
     {
         [Category("Connection")]
         [Description("Specify Client Object, Otherwise Uses Scope.")]
         [DisplayName("Graph Client")]
         public InArgument<GraphServiceClient> GraphClient { get; set; }
-
-        protected abstract void ReadContext(AsyncCodeActivityContext context);
-        protected abstract Task Initialize(GraphServiceClient client, AsyncCodeActivityContext context, CancellationToken token);
 
         protected override async Task<Action<AsyncCodeActivityContext>> ExecuteAsync(
           AsyncCodeActivityContext context,
@@ -33,24 +30,28 @@ namespace Impower.Office365
         {
             //HANDLE CLIENT
             var client = context.GetValue(GraphClient);
-            if(client == null)
+            if (client == null)
             {
                 client = (GraphServiceClient)Extensions.GetClientFromScope(context);
             }
-            if(client == null)
+            if (client == null)
             {
                 throw new Exception("Could not acquire Graph Client from context. Place activity in scope or pass in client directly.");
             }
-            
+
             //BEGIN EXECUTION
             ReadContext(context);
             await Initialize(client, context, token);
             return await ExecuteAsyncWithClient(token, client);
-
         }
+
         protected abstract Task<Action<AsyncCodeActivityContext>> ExecuteAsyncWithClient(
           CancellationToken token,
           GraphServiceClient client
         );
+
+        protected abstract Task Initialize(GraphServiceClient client, AsyncCodeActivityContext context, CancellationToken token);
+
+        protected abstract void ReadContext(AsyncCodeActivityContext context);
     }
 }
--- Sharepoint/Activities/UploadListItems.cs
+++ Sharepoint/Activities/UploadListItems.cs
@@ -13,12 +13,26 @@ namespace Impower.Office365.Sharepoint
 {
     public class UploadListItems : SharepointSiteActivity
     {
+        private DataTable data;
+
+        private string listID;
+
+        [RequiredArgument]
+        public InArgument<DataTable> Data { get; set; }
+
         [RequiredArgument]
         public InArgument<ListLocator> List { get; set; }
-        private string listID;
-        [RequiredArgument]       
-        public InArgument<DataTable> Data { get; set; }
-        private DataTable data;
+
+        protected override async Task<Action<AsyncCodeActivityContext>> ExecuteAsyncWithClient(CancellationToken token, GraphServiceClient client)
+        {
+            await client.UploadListItems(token, SiteId, listID, data);
+            return ctx => { };
+        }
+
+        protected override Task Initialize(GraphServiceClient client, AsyncCodeActivityContext context, CancellationToken token)
+        {
+            return base.Initialize(client, context, token);
+        }
 
         protected override void ReadContext(AsyncCodeActivityContext context)
         {
@@ -26,15 +40,5 @@ namespace Impower.Office365.Sharepoint
             listID = List.Get(context);
             data = Data.Get(context);
         }
-        protected override async Task<Action<AsyncCodeActivityContext>> ExecuteAsyncWithClient(CancellationToken token, GraphServiceClient client)
-        {
-            await client.UploadListItems(token, SiteId, listID, data);
-            return ctx => { };
-        }
-        protected override Task Initialize(GraphServiceClient client, AsyncCodeActivityContext context, CancellationToken token)
-        {
-            return base.Initialize(client, context, token);
-        }
-
     }
 }
